4ab88e2e804d4134c5c8d02a6ffc5cf3b7a4c747
Trevor Powell <trevor@vectorstorm.org>
2021-08-06 13:07:30 +1000

    Reimplements WPM feature.
     - Now calculates exact WPM over the last up to three seconds of typing.
     - WPM_SMOOTHING removed, as it's no longer needed.
     - WPM_SAMPLE_SECONDS added, to specify how long a period to average WPM
       over, set to 5 seconds by default.
     - WPM_UNFILTERED option added (defaults to unset), which disables all
       filtering within the WPM feature.  This saves some space in the
       firmware and also reduces latency between typing and the WPM
       calculation measuring it.  (saves 70 bytes in my tests)
     - WPM_LAUNCH_CONTROL added (defaults to unset).  When typing begins
       while the current displayed WPM value is zero, the WPM calculation
       only considers the time elapsed since typing began, not the whole
       WPM_SAMPLE_SECONDS buffer.  The result of this is that the displayed
       WPM value much more rapidly reaches an accurate WPM value, even when
       results are being filtered. (costs 22 bytes in my tests)
     - Updates documentation to reflect changed options.
    
    Saves 1162 bytes, in my tests, compared against the previous implementation,
    with default settings.

--- quantum/wpm.c
+++ quantum/wpm.c
@@ -23,11 +23,31 @@
 static uint8_t  current_wpm = 0;
 static uint16_t wpm_timer   = 0;
 
-// This smoothing is 40 keystrokes
-static const float wpm_smoothing = WPM_SMOOTHING;
+/* The WPM calculation works by specifying a certain number of 'periods' inside
+ * a ring buffer, and we count the number of keypresses which occur in each of
+ * those periods.  Then to calculate WPM, we add up all of the keypresses in
+ * the whole ring buffer, divide by the number of keypresses in a 'word', and
+ * then adjust for how much time is captured by our ring buffer.  Right now
+ * the ring buffer is hardcoded below to be six half-second periods, accounting
+ * for a total WPM sampling period of up to three seconds of typing.
+ *
+ * Whenever our WPM drops to absolute zero due to no typing occurring within
+ * any contiguous three seconds, we reset and start measuring fresh,
+ * which lets our WPM immediately reach the correct value even before a full
+ * three second sampling buffer has been filled.
+ */
+#define MAX_PERIODS (10)
+#define PERIOD_DURATION (1000 * WPM_SAMPLE_SECONDS / MAX_PERIODS)
+static int8_t  period_presses[MAX_PERIODS] = {0};
+static uint8_t current_period              = 0;
+static uint8_t periods                     = 1;
 
-void set_current_wpm(uint8_t new_wpm) { current_wpm = new_wpm; }
+#if !defined(WPM_UNFILTERED)
+static uint8_t prev_wpm = 0;
+static uint8_t next_wpm = 0;
+#endif
 
+void    set_current_wpm(uint8_t new_wpm) { current_wpm = new_wpm; }
 uint8_t get_current_wpm(void) { return current_wpm; }
 
 bool wpm_keycode(uint16_t keycode) { return wpm_keycode_kb(keycode); }
@@ -68,33 +88,55 @@ __attribute__((weak)) uint8_t wpm_regress_count(uint16_t keycode) {
 }
 #endif
 
+// Outside 'raw' mode we smooth results over time.
+
 void update_wpm(uint16_t keycode) {
     if (wpm_keycode(keycode)) {
-        if (wpm_timer > 0) {
-            uint16_t latest_wpm = 60000 / timer_elapsed(wpm_timer) / WPM_ESTIMATED_WORD_SIZE;
-            if (latest_wpm > UINT8_MAX) {
-                latest_wpm = UINT8_MAX;
-            }
-            current_wpm += ceilf((latest_wpm - current_wpm) * wpm_smoothing);
-        }
-        wpm_timer = timer_read();
+        period_presses[current_period]++;
     }
 #ifdef WPM_ALLOW_COUNT_REGRESSION
     uint8_t regress = wpm_regress_count(keycode);
     if (regress) {
-        if (current_wpm < regress) {
-            current_wpm = 0;
-        } else {
-            current_wpm -= regress;
-        }
-        wpm_timer = timer_read();
+        period_presses[current_period]--;
     }
 #endif
 }
 
 void decay_wpm(void) {
-    if (timer_elapsed(wpm_timer) > 1000) {
-        current_wpm += (-current_wpm) * wpm_smoothing;
-        wpm_timer = timer_read();
+    int presses = period_presses[0];
+    for (int i = 1; i < periods; i++) {
+        presses += period_presses[i];
     }
+    int wpm_now = (60000 * presses) / (periods * PERIOD_DURATION * WPM_ESTIMATED_WORD_SIZE);
+    int elapsed = timer_elapsed(wpm_timer);
+
+    if (elapsed > PERIOD_DURATION) {
+#ifndef WPM_UNFILTERED
+        prev_wpm = current_wpm;
+        next_wpm = wpm_now;
+#endif
+
+        current_period                 = (current_period + 1) % MAX_PERIODS;
+        period_presses[current_period] = 0;
+        periods                        = (periods < MAX_PERIODS) ? periods + 1 : MAX_PERIODS;
+        elapsed                        = 0;
+        /* if (wpm_timer == 0) { */
+        wpm_timer = timer_read();
+        /* } else { */
+        /*     wpm_timer += PERIOD_DURATION; */
+        /* } */
+    }
+
+#if defined WPM_LAUNCH_CONTROL
+    if (presses == 0) {
+        current_period = 0;
+        periods        = 1;
+    }
+#endif  // WPM_LAUNCH_CONTROL
+
+#ifndef WPM_UNFILTERED
+    current_wpm = prev_wpm + (elapsed * (next_wpm - prev_wpm) / PERIOD_DURATION);
+#else
+    current_wpm = wpm_now;
+#endif
 }
