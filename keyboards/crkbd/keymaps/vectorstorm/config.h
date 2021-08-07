5eee792ccb305657128c60ee9df48aca20b1c638
Trevor Powell <trevor@vectorstorm.org>
2021-08-07 14:24:54 +1000

    RLE-encoded bongo cat.
    Configured bongo cat onto slave side, with WPM sync between halves.

---
+++ keyboards/crkbd/keymaps/vectorstorm/config.h
@@ -0,0 +1,104 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define PERMISSIVE_HOLD
+#undef IGNORE_MOD_TAP_INTERRUPT
+#define USB_POLLING_INTERVAL_MS 1
+#define WPM_UNFILTERED
+#define WPM_ESTIMATED_WORD_SIZE 6
+#define WPM_SAMPLE_SECONDS 2
+// #define WPM_SAMPLE_SECONDS 10
+#define WPM_LAUNCH_CONTROL
+#define SPLIT_WPM_ENABLE
+
+// #define NO_ACTION_ONESHOT
+// #define NO_ACTION_TAPPING
+
+#define OLED_UPDATE_INTERVAL 50
+
+
+#ifdef RGB_MATRIX_ENABLE
+#define RGBLIGHT_ANIMATIONS
+// #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+// #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+
+/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
+ * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
+// #   define ENABLE_RGB_MATRIX_ALPHAS_MODS
+// #   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#   define ENABLE_RGB_MATRIX_BREATHING
+// #   define ENABLE_RGB_MATRIX_CYCLE_ALL
+#   define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#   define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+// #   define ENABLE_RGB_MATRIX_DUAL_BEACON
+#   define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#   define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #   define ENABLE_RGB_MATRIX_RAINDROPS
+// #   define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+// #   define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #   define ENABLE_RGB_MATRIX_SPLASH
+// #   define ENABLE_RGB_MATRIX_MULTISPLASH
+// #   define ENABLE_RGB_MATRIX_SOLID_SPLASH
+// #   define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#define LAYER_STATE_8BIT
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
